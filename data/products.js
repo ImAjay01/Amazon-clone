fee72778cc3104c6d29478a2f8d6acc2b75b463e
Ajay Singh Patel <asp44556@gmail.com>
2024-11-25 21:51:46 +0530

    learn promises and convert code to promises

--- data/products.js
+++ data/products.js
@@ -66,16 +66,16 @@ export let products = [];
 
 export function loadProducts(fun){
   const xhr = new XMLHttpRequest();
-
+  
   xhr.addEventListener('load', () => {
     products = JSON.parse(xhr.response).map((productDetails)=> {
-        if(productDetails.type === 'clothing'){
-          return new Clothing(productDetails);
-        }
-       return new Product(productDetails);
-      });
-      console.log(loadProducts)
-      fun();
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load Products')
+    fun();
   })
   xhr.open('GET', 'https://supersimplebackend.dev/products')
   xhr.send();
